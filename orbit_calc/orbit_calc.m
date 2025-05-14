d4ac217417079121d4f45022209a07730709f2f8
Jordan Mandel <jordanmandel@Jordans-MacBook-Pro-3.local>
2025-05-14 11:24:59 -0400

    orbits

--- orbit_calc/orbit_calc.m
+++ orbit_calc/orbit_calc.m
@@ -54,3 +54,37 @@ for p in polys do
 end for;
 
 #orbitsList;
+
+W,g:=GModule(G,R,6);
+
+uniqueOrbitPolys := [];
+for w in W do
+    Append(~uniqueOrbitPolys, w);
+end for;
+#uniqueOrbitPolys;
+
+for w in W do
+    orbit := [];
+    if w in uniqueOrbitPolys then
+        Append(orbit, w);
+        for a in uniqueOrbitPolys do
+            for M in G do
+                for c in polys do
+                    term := a*M;
+                    termPoly := term@@f;
+                    termPoly := termPoly*c + c*c;
+                    term2 := w*M;
+                    termPoly := term2@@g + termPoly;
+                    orbitp := g(termPoly);
+                end for;
+            end for;
+            i := Index(uniqueOrbitPolys, orbitp);
+            if i ne 0 then
+                Remove(~uniqueOrbitPolys, i);
+            end if;
+        end for;
+        Append(~orbitsList, orbit);
+    end if;
+end for;
+
+#orbitsList;
