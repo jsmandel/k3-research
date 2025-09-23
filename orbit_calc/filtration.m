f192b20c70ed0cd1bdf2278f0efa6a7e2029f6f7
Jordan Mandel <jordanmandel@Jordans-MacBook-Pro-3.local>
2025-09-23 15:51:33 -0400

    filtration with (a,b) pairs

---
+++ orbit_calc/filtration.m
@@ -0,0 +1,65 @@
+k := FiniteField(2);
+G := GL(3, k);
+R<[x]> := PolynomialRing(k,3);
+V, f := GModule(G, R, 3);
+GV := ActionGroup(V);
+
+// Computes the orbits of G on the degree 3 polynomials
+GVorbits := Orbits(GV);
+
+// Returns a list of orbit representatives, as vectors
+GVorbreps := [GVorbits[i][1] : i in [1..#GVorbits]];
+
+// Returns a list of orbit representatives, as polynomials
+GVorbrepspoly := [(GVorbreps[i])@@f : i in [1..#GVorbreps]];
+
+orbitsList := [**];
+orbitRepList := [**];
+num_orbits := 0;
+
+for a in GVorbreps do
+    ap := a@@f;
+    
+    stabA := Stabilizer(GV, a); // stabilizer of a
+    gvMap := GModuleAction(V); // map from G to GV
+    stabG := stabA @@ gvMap; // makes the stabilizer subgroup as a subgroup of G
+
+    // stabilizer subgroup acting on W
+    if not IsTrivial(stabG) then
+        W, g := GModule(stabG, R, 6);
+        GW := ActionGroup(W);
+
+        Wa := sub<W | [g((a@@f)*(c@@f) + (c@@f)^2) : c in V]>;
+        Q, q := quo<W | Wa>;
+        GQ := ActionGroup(Q);
+
+        // list of all orbits of group action
+        orbit := Orbits(GQ);
+
+        // adds one orbit representative from each orbit to list
+        for rep in orbit do
+            orbitRep := [*a*];
+            Append(~orbitRep, orbit[1]);
+            Append(~orbitRepList, orbitRep);
+        end for;
+        Append(~orbitsList, orbit);
+        num_orbits := num_orbits + #orbit;
+    else
+        Append(~orbitsList, a);
+        Append(~orbitRep, Random(W));
+        Append(~orbitRepList, orbitRep);
+        num_orbits := num_orbits + 1;
+    end if;
+
+end for;
+
+// Orbit List
+// f := Open("orbitList2.m", "w");
+// Puts(f, "orbitsList := " cat Sprint(orbitsList, "Magma") cat ";");
+// Flush(f);
+
+// Orbit Rep List
+f := Open("orbitReps.m", "w");
+Puts(f, "orbitRepList := " cat Sprint(orbitRepList, "Magma") cat ";");
+Flush(f);
+num_orbits;
